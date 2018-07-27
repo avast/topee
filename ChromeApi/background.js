53ecc8ad9b6dcb04b1c3eba39f7fc0326a142acb
Studený Pavel <studeny@avast.com>
2018-07-27 15:32:52 +0200

    bundle Background in background.js

---
+++ ChromeApi/background.js
@@ -0,0 +1,14 @@
+
+(function () {
+'use strict';
+
+  if (!window.chrome) {
+    window.chrome = {};
+  };
+ 
+  window.chrome.extension = {
+    getURL: function(path) {
+      return window.location.origin + path;
+    }
+  };
+})();
