b6200656a672563c2a1be3bb2f8d564ea62651bd
Felix J. Acero <acerosalazar@avast.com>
2018-07-30 13:54:31 +0200

    Adds Appex target

---
+++ src/Demo/Appex/Sources/SafariExtensionViewController.swift
@@ -0,0 +1,11 @@
+//
+//  Copyright © 2018 Avast. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
