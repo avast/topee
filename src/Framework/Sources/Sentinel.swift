438eab97cc768d02d054a81056f377e55a513cff
Felix J. Acero <acerosalazar@avast.com>
2018-07-30 13:29:31 +0200

    Adds framework project

---
+++ src/Framework/Sources/Sentinel.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2018 Avast. All rights reserved.
+//
+
+import Foundation
+
+public enum Sentinel {
+    case empty
+}
---
+++ src/Framework/Support/Sources/Topee.h
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2018 Avast. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Topee.
+FOUNDATION_EXPORT double TopeeVersionNumber;
+
+//! Project version string for Topee.
+FOUNDATION_EXPORT const unsigned char TopeeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Topee/PublicHeader.h>
