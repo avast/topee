ae5e4d80313f6b669c4c992a28677ec0695284fb
Felix J. Acero <acerosalazar@avast.com>
2018-07-30 14:34:19 +0200

    Links the Topee framework to the Appex

--- src/Demo/Appex/Sources/SafariExtensionHandler.swift
+++ src/Demo/Appex/Sources/SafariExtensionHandler.swift
@@ -2,11 +2,13 @@
 //  Copyright © 2018 Avast. All rights reserved.
 //
 
+import Topee
 import SafariServices
 
 class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
+        let _: Sentinel = .empty
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
         page.getPropertiesWithCompletionHandler { properties in
             NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
