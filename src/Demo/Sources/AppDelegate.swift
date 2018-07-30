c887b1d1ab53cac34ad5cf8cb32ac3aa4774f3fb
Felix J. Acero <acerosalazar@avast.com>
2018-07-30 13:15:59 +0200

    Adds demo project

---
+++ src/Demo/Sources/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Felix J. Acero on 30/07/2018.
+//  Copyright © 2018 Avast. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
