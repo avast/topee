6c5842e3b1c60895b582671f320979af5ab587f4
Studený Pavel <studeny@avast.com>
2018-07-31 08:16:42 +0200

    cross origin iframe test case

--- test/PageSamples/http.sh
+++ test/PageSamples/http.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd `dirname "$0"`
+echo Starting HTTP server in `pwd` on http://localhost:8001
+python -m SimpleHTTPServer 8001 &
 echo Starting HTTP server in `pwd` on http://localhost:8000
-python -m SimpleHTTPServer
+python -m SimpleHTTPServer 8000
+kill `jobs -p`
