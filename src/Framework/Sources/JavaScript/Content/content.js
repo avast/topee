e122bf0e09b75c8a146fb979f59f1d1844ae095b
Studený Pavel <studeny@avast.com>
2018-08-01 17:06:00 +0200

    Document reason for pageshow listener in content.js

--- src/Framework/Sources/JavaScript/Content/content.js
+++ src/Framework/Sources/JavaScript/Content/content.js
@@ -17,6 +17,8 @@ else {
 }
 
 window.addEventListener("pageshow", function(event) {
+    // When user navigates back Safari ressurects page so we need to trigger hello also in
+    // this case (because was dereferenced using beforeunload)
     tabInfo.tabId.then(tabId => {
         safari.extension.dispatchMessage('hello', {
             tabId: tabId,
