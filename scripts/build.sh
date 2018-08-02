68eb6947cdc00dd0607b92816ae5ec6d27041f67
Felix J. Acero <acerosalazar@avast.com>
2018-08-02 15:11:07 +0200

    Cleans the build directory before every build

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,12 @@ set -o pipefail
 function main() {
     local -r SELF_DIR="$(cd "$(dirname "$0")"; pwd)"
     local -r BUILD_DIR="${SELF_DIR}/../build"
+    rm -rf "$BUILD_DIR/*"
     pushd "$SELF_DIR" > /dev/null
     xcodebuild -workspace ../src/Topee.xcworkspace -scheme Topee clean build CONFIGURATION=Release BUILD_DIR="$BUILD_DIR"
     pushd "$BUILD_DIR/Release" > /dev/null
     cp "${SELF_DIR}/../src/Framework/Build/content.js" .
-    zip -r "Topee.framework.zip" .
+    zip -yr "Topee.framework.zip" .
     popd > /dev/null
     popd > /dev/null
 }
