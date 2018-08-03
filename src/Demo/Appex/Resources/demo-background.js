97a53526b61fb5f96dd30ef34c542929027ca54a
Studený Pavel <studeny@avast.com>
2018-08-03 12:36:14 +0200

    simple test setup

--- src/Demo/Appex/Resources/demo-background.js
+++ src/Demo/Appex/Resources/demo-background.js
@@ -1,6 +1,78 @@
 var bgPingCount = 0;
 
+var activeListeners = {};
+function setupListeners(listeners, namespace) {
+    if (!namespace) {
+        namespace = window;
+    }
+
+    var listenerIds = {};
+
+    for (var key in listeners) {
+        if (typeof namespace[key] === 'function' && key === 'addListener') {
+            listenerIds[key] = installListener(namespace[key], listeners[key]);
+        }
+        else if (typeof namespace[key] === 'function') {
+            namespace[key].apply(namespace, listeners[key].arguments);
+        }
+        else {
+            listenerIds[key] = setupListeners(listeners[key], namespace[key]);
+        }
+    }
+
+    return listenerIds;
+
+    function installListener(f, context) {
+        var id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+        activeListeners[id] = {
+            listener: function (message, sender, sendResponse) {
+                if (context[message.type]) {
+                    message.processed = true;
+                    sendResponse(context[message.type]);
+                }
+            },
+            type: Object.keys(context)[0]
+        };
+        f(activeListeners[id].listener);
+        return id;
+    }
+}
+
+function shutdownListeners(listeners, namespace) {
+    if (!namespace) {
+        namespace = window;
+    }
+
+    var count = 0;
+    
+    for (var key in listeners) {
+        if (typeof namespace[key] === 'function' && key === 'addListener') {
+            ++count;
+            namespace.removeListener(activeListeners[listeners[key]].listener);
+            delete activeListeners[listeners[key]];
+        }
+        else if (typeof namespace[key] !== 'function') {
+            count += shutdownListeners(listeners[key], namespace[key]);
+        }
+    }
+
+    return count;
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.type === 'jasmine.setup') {
+        sendResponse(setupListeners(message.value));
+        return;
+    }
+    if (message.type === 'jasmine.shutdown') {
+        sendResponse(shutdownListeners(message.value));
+        return;
+    }
+    if (message.type && Object.values(activeListeners).some(l => l.type === message.type)) {
+        return;
+    }
+            
+    
     sendResponse("background pong #" + message.value);
 
     setTimeout(function () {
@@ -9,3 +81,4 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     	})
     }, 200);
 });
+
---
+++ src/Demo/Appex/Resources/test.js
@@ -0,0 +1,41 @@
+describe("jasmine setup", function () {
+  it("sets and shuts down listeners", function (done) {
+    chrome.runtime.sendMessage({ type: "jasmine.setup", value: {
+      chrome: {
+        runtime: {
+          onMessage: {
+            addListener: {
+              testMessage: "testResponse"
+            }
+          }
+        }
+      }
+    }}, function (resp) {
+      expect(Object.keys(resp).length).toBe(1);
+      chrome.runtime.sendMessage({ type: "jasmine.shutdown", value: resp}, function (resp) {
+        expect(resp).toBe(1);
+        done();
+      });
+    });
+  });
+
+  it("installs listeners correctly", function (done) {
+    chrome.runtime.sendMessage({ type: "jasmine.setup", value: {
+      chrome: {
+        runtime: {
+          onMessage: {
+            addListener: {
+              testMessage: "testResponse"
+            }
+          }
+        }
+      }
+    }}, function (listeners) {
+      chrome.runtime.sendMessage({ type: "testMessage"}, function(resp) {
+        expect(resp).toBe("testResponse");
+        chrome.runtime.sendMessage({ type: "jasmine.shutdown", value: listeners});
+        done();
+      });
+    });
+  });
+});
