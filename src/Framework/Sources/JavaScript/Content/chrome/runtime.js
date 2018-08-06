c3d8b4424d4b234e20c77b8b528f1a066eb57449
Studený Pavel <studeny@avast.com>
2018-08-06 10:14:46 +0200

    set response listener before the request is sent

--- src/Framework/Sources/JavaScript/Content/chrome/runtime.js
+++ src/Framework/Sources/JavaScript/Content/chrome/runtime.js
@@ -10,7 +10,12 @@ var eventEmitter = new EventEmitter();
 // TODO: once tabId is fulfilled, sendMessage should call dispatchMessage right away, not in then(), that might be performed asynchronously
 runtime.sendMessage = function(message, callback) {
 	var messageId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-	tabInfo.tabId.then(tabId => {
+
+    if (callback) {
+    	safari.self.addEventListener("message", listener);
+    }
+
+    tabInfo.tabId.then(tabId => {
 		safari.extension.dispatchMessage('request', {
         	tabId: tabId,
         	payload: JSON.stringify({
@@ -23,10 +28,6 @@ runtime.sendMessage = function(message, callback) {
         	})
     	});
     });
-
-    if (callback) {
-    	safari.self.addEventListener("message", listener);
-    }
 
     function listener(event) {
     	if (event.name === 'response' && event.message.messageId === messageId) {
