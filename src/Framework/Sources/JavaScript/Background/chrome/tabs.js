7c4509990980bf42038e15a64b1af877cacc9990
Marian Schubert <marian.schubert@gmail.com>
2018-08-06 13:19:28 +0200

    Cleanup chrome.tabs

--- src/Framework/Sources/JavaScript/Background/chrome/tabs.js
+++ src/Framework/Sources/JavaScript/Background/chrome/tabs.js
@@ -5,56 +5,58 @@ var urlMatcher = require('../url-matcher.js');
 
 var tabs = {};
 
+// Internal state management
+var browserTabs = {};
+
+eventEmitter.addListener('hello', function (payload) {
+    if (payload.frameId !== 0) { return; }
+    browserTabs[payload.tabId] = {
+        id: payload.tabId,
+        url: payload.url
+    };
+});
+
+// chrome.tabs API
 tabs.sendMessage = function (tabId, message, options, responseCallback) {
-	var messageId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    var messageId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
 
-	if (typeof options === 'function') {
-		responseCallback = options;
-	}
+    if (typeof options === 'function') {
+        responseCallback = options;
+    }
 
-	if (responseCallback) {
-		eventEmitter.addListener('messageResponse', onResponse);
-	}
+    if (responseCallback) {
+        eventEmitter.addListener('messageResponse', onResponse);
+    }
 
     window.webkit.messageHandlers.content.postMessage({
         tabId: tabId,
         eventName: 'request',
         frameId: (options && options.frameId) || undefined,
-	    messageId: messageId,
-    	payload: message
+        messageId: messageId,
+        payload: message
     });
 
     function onResponse(payload) {
-    	if (payload.messageId === messageId) {
-    		responseCallback(payload.message);
-    		eventEmitter.removeListener('messageResponse', onResponse);
-    	}
+        if (payload.messageId === messageId) {
+            responseCallback(payload.message);
+            eventEmitter.removeListener('messageResponse', onResponse);
+        }
     }
 };
 
-var browserTabs = {};
-
-eventEmitter.addListener('hello', function (payload) {
-	if (payload.frameId !== 0) { return; }
-	browserTabs[payload.tabId] = {
-		id: payload.tabId,
-		url: payload.url
-	};
-});
-
 tabs.query = function(queryInfo, callback) {
-	var tabs = [];
-	for (var tab in browserTabs) {
-		tabs.push(browserTabs[tab]);	
-	}
-	if (queryInfo.url) {
-		tabs = tabs.filter(tab => urlMatcher.match(queryInfo.url, tab.url));
-	}
-	callback(tabs);
-}
+    var tabs = [];
+    for (var tab in browserTabs) {
+        tabs.push(browserTabs[tab]);
+    }
+    if (queryInfo.url) {
+        tabs = tabs.filter(tab => urlMatcher.match(queryInfo.url, tab.url));
+    }
+    callback(tabs);
+};
 
 tabs.sendMessage._emit = function (payload) {
-	eventEmitter.emit('messageResponse', payload);
+    eventEmitter.emit('messageResponse', payload);
 };
 
 module.exports = tabs;
