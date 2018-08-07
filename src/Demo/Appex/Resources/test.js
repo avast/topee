ff0acbff28d4b1548d0e80f3798cb338fb6ad8ca
Studený Pavel <studeny@avast.com>
2018-08-07 16:35:50 +0200

    simple chrome.tabs.query test

--- src/Demo/Appex/Resources/test.js
+++ src/Demo/Appex/Resources/test.js
@@ -40,5 +40,65 @@ describe('jasmine setup', function () {
       done();
     });
   });
+});
 
+describe('chrome.tabs', function () {
+  describe('query', function () {
+    var initialTabs;
+    var openTab;
+    var cit = popupsEnabled() ? it : xit;
+
+    function popupsEnabled() {
+      if (!window.location.protocol.toLowerCase().startsWith('http')) {  // prevent infinite window.open() loop
+        return false;
+      }
+      var w = window.open();
+      if (w) {
+        w.close();
+      }
+
+      return !!w;
+    }
+
+    beforeEach(function (done) {
+      openTab = null;
+    });
+
+    afterEach(function () {
+      if (openTab) {
+        openTab.close();
+        openTab = null;
+      }
+    });
+
+    cit('recognizes a new tab', function (done) {
+      chrome.runtime.sendMessage({ type: 'test.backgroundInvoke', value: {
+        name: 'chrome.tabs.query',
+        arguments: {},
+        wantCallback: true
+      }}, function (result) {
+        console.log('got tabs', result);
+        initialTabs = result;
+
+        var url = 'https://raw.githubusercontent.com/avast/topee/master/README.md?q=' + (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)).toString();
+        openTab = window.open();
+        // addEventListener('load') does not trigger in this case
+        setTimeout(function () {
+          console.log('openTab loaded');
+          chrome.runtime.sendMessage({ type: 'test.backgroundInvoke', value: {
+            name: 'chrome.tabs.query',
+            arguments: {},
+            wantCallback: true
+          }}, function (result) {
+            console.log('updated tabs', result);
+            expect(result.find(tab => tab.url === url)).not.toBeNull();
+            expect(result.length).toBe(initialTabs.length + 1);
+            done();
+          });
+        }, 100);
+        console.log('assigning location to', url);
+        openTab.location = url;
+      });
+    });
+  });
 });
