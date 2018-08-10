1525da339358e9d68682dabb5b5e61c0068881f0
Pavel Studeny <studeny@avast.com>
2018-08-10 11:42:58 +0200

    simple AES-CBC encryption based on crypto.subtle

---
+++ src/Framework/Sources/JavaScript/aes-crypto.js
@@ -0,0 +1,66 @@
+// see https://github.com/diafygi/webcrypto-examples#aes-cbc---generatekey
+
+function createSalt() {
+  return crypto.getRandomValues(new Uint8Array(16));  // direct value, not a promise
+}
+
+function createKey() {
+  return crypto.subtle.generateKey(
+    {
+      name: 'AES-CBC',
+      length: 256
+    },
+    true,
+    [ 'encrypt', 'decrypt' ]);
+}
+
+function importKey(jwkKey) {
+  return crypto.subtle.importKey(
+    "jwk",
+    {
+        kty: "oct",
+        k: jwkKey,
+        alg: "A256CBC",
+        ext: true,
+    },
+    {
+        name: "AES-CBC",
+    },
+    true,
+    ["encrypt", "decrypt"]);
+}
+
+function exportKey(key) {
+  return crypto.subtle.exportKey(
+    "jwk",
+    key).then(jwkKey => jwkKey.k);
+}
+
+function encrypt(arrayBufferata, ui8aSalt, key) {
+  return crypto.subtle.encrypt(
+    {
+      name: 'AES-CBC',
+      iv: ui8aSalt
+    },
+    key,
+    arrayBufferata);
+}
+
+function decrypt(arrayBufferData, ui8aSalt, key) {
+  return crypto.subtle.decrypt(
+    {
+      name: 'AES-CBC',
+      iv: ui8aSalt
+    },
+    key,
+    arrayBufferData);
+}
+
+module.exports = {
+  createSalt: createSalt,
+  createKey: createKey,
+  importKey: importKey,
+  exportKey: exportKey,
+  encrypt: encrypt,
+  decrypt: decrypt
+};
---
+++ src/Framework/Sources/JavaScript/binary-conversion.js
@@ -0,0 +1,46 @@
+function uint8array2str(ua) {
+  return String.fromCharCode.apply(null, ua);
+}
+
+function arrayBuffer2str(ab) {
+  return uint8array2str(new Uint8Array(ab));
+}
+
+function str2uint8array(s) {
+  return new Uint8Array([].map.call(s,function(x){var c = x.charCodeAt(0); if (c > 0xFF) throw s + ': cannot convert non-ASCII character'; return c; }));
+}
+
+function str2arrayBuffer(s) {
+  return str2uint8array(s).buffer; 
+}
+
+
+function uint8array2base64(ua) {
+  return btoa(uint8array2str(ua)); 
+}
+
+function arrayBuffer2base64(ab) {
+  return btoa(arrayBuffer2str(ab)); 
+}
+
+function base642uint8array(b64) {
+  return str2uint8array(atob(b64));
+}
+
+function base642arrayBuffer(b64) {
+  return str2arrayBuffer(atob(b64));
+}
+
+module.exports = {
+  uint8array2str: uint8array2str,
+  arrayBuffer2str: arrayBuffer2str,
+  
+  str2uint8array: str2uint8array,
+  str2arrayBuffer: str2arrayBuffer,
+  
+  uint8array2base64: uint8array2base64,
+  arrayBuffer2base64: arrayBuffer2base64,
+  
+  base642uint8array: base642uint8array,
+  base642arrayBuffer: base642arrayBuffer
+};
