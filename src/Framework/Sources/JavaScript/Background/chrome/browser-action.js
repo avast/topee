84555fa96c86eb20c9e3e481b614923fa8530e0f
Marian Schubert <marian.schubert@gmail.com>
2018-08-17 17:34:20 +0200

    Implement chrome.browserAction.onClicked.addListener

--- src/Framework/Sources/JavaScript/Background/chrome/browser-action.js
+++ src/Framework/Sources/JavaScript/Background/chrome/browser-action.js
@@ -1,19 +1,33 @@
 // https://developer.chrome.com/extensions/browserAction
+var eventEmitter = require('../event-bus.js');
 
 var browserAction = {};
+
+var state = {
+    popup: {}
+};
 
 // TODO: Implementation
 browserAction.setTitle = function () {};
 
-// TODO: Implementation
-browserAction.setPopup = function () {};
+// TODO: Implementation (actual display of popup + callback handling is missing)
+browserAction.setPopup = function ({tabId, popup}, callback) {
+    state.popup = {tabId, popup, callback};
+};
 
 // TODO: Implementation
 browserAction.setIcon = function () {};
 
 browserAction.onClicked = {
-    // TODO: Implementation
-    addListener: function () {}
+    addListener: function (fn) {
+        eventEmitter.addListener('toolbarItemClicked', function () {
+            // Only call fn if popup isn't defined
+            if (!state.popup.popup) {
+                // TODO: Also check tabId
+                fn();
+            }
+        });
+    }
 };
 
 module.exports = browserAction;
