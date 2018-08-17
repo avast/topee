08fdbe27192bc8b786177746c8ed7c59e0bd6125
Studený Pavel <studeny@avast.com>
2018-08-17 10:02:13 +0200

    update resources

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,8 @@ function main() {
     pushd "$SELF_DIR" > /dev/null
     xcodebuild -workspace ../src/Topee.xcworkspace -scheme Topee clean build CONFIGURATION=Release BUILD_DIR="$BUILD_DIR"
     pushd "$BUILD_DIR/Release" > /dev/null
-    cp "${SELF_DIR}/../src/Framework/Build/content.js" .
+    cp "${SELF_DIR}/../src/Framework/Build/topee-content.js" .
+    cp "${SELF_DIR}/../src/Framework/Build/topee-iframe-resources.js" .
     zip -yr "Topee.framework.zip" .
     popd > /dev/null
     popd > /dev/null
