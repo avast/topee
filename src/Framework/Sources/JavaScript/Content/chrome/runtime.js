3566f6c39a3ea92c0db9ad8c3550ba7063d72897
Marian Schubert <marian.schubert@gmail.com>
2018-09-06 16:17:19 +0200

    Use background.dispatchRequest

--- src/Framework/Sources/JavaScript/Content/chrome/runtime.js
+++ src/Framework/Sources/JavaScript/Content/chrome/runtime.js
@@ -51,16 +51,11 @@ runtime.onMessage = {
 safari.self.addEventListener("message", function (event) {
     // message from the background script and a response
     if (event.name === 'request' && (!event.message.frameId || event.message.frameId === tabInfo.frameId)) {
-        tabInfo.tabId.then(tabId => {
-            eventEmitter.emit('message', event.message.payload, {id: 'topee'}, function (message) {
-                safari.extension.dispatchMessage('request', {
-                    tabId: tabId,
-                    payload: {
-                        eventName: 'messageResponse',
-                        messageId: event.message.messageId,
-                        message: message
-                    }
-                });
+        eventEmitter.emit('message', event.message.payload, {id: 'topee'}, function (message) {
+            background.dispatchRequest({
+                eventName: 'messageResponse',
+                messageId: event.message.messageId,
+                message: message
             });
         });
     }
