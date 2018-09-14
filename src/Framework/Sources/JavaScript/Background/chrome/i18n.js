b690615b83d8eba63b0666fb39c2241099611432
Marian Schubert <marian.schubert@gmail.com>
2018-09-14 14:49:20 +0200

    Make i18n.getMessage handle missing locale gracefuly

--- src/Framework/Sources/JavaScript/Background/chrome/i18n.js
+++ src/Framework/Sources/JavaScript/Background/chrome/i18n.js
@@ -10,35 +10,35 @@ var DEFAULT_LOC = 'en';
 
 function getLocale() {
     var langCode = i18n.getUILanguage().replace(/-/g, '_');
-    
+
     if (i18n._locales[langCode]) {
         return langCode;
     }
-    
+
     var parts = langCode.split('_');
     var shortCode = parts.shift().toLowerCase();
     var loUP = shortCode + '_' + parts.map(function (p) { return p.toUpperCase(); }).join('_');
-    
+
     if (i18n._locales[loUP]) {
         return loUP;
     }
-    
+
     if (i18n._locales[shortCode]) {
         return shortCode;
     }
-    
+
     var long = Object.keys(i18n._locales).find(function (l) { return l.startsWith(shortCode); });
     if (long) {
         return long;  // this would return e.g. pt_BR for pt. not that great, but better than en
     }
-    
+
     return DEFAULT_LOC;
 }
 
 i18n.getMessage = function (messageName) {
-    var translation = i18n._locales[getLocale()][messageName];
+    var translation = i18n._locales[getLocale()] && i18n._locales[getLocale()][messageName];
     if (!translation || typeof translation.message === 'undefined') {
-        return '';
+        return messageName;
     }
     return translation.message;
 };
