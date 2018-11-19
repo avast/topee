16010e3f8c9cca91edc66721bef4581e0398e5ba
Marian Schubert <marian.schubert@gmail.com>
2018-11-19 12:50:33 +0100

    Dummy implementation of chrome.commands.onCommand

---
+++ src/Framework/Sources/JavaScript/Background/chrome/commands.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const commands = {};
+
+commands.onCommand = {
+    addListener: function () {
+        console.warn('commands.onCommand.addListener is not implemented.');
+    },
+    removeListener: function() {
+        console.warn('commands.onCommand.removeListener is not implemented.');
+    },
+};
+
+module.exports = commands;
--- src/Framework/Sources/JavaScript/Background/chrome/index.js
+++ src/Framework/Sources/JavaScript/Background/chrome/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	browserAction: require('./browser-action.js'),
 	contextMenus: require('./context-menus.js'),
+	commands: require('./commands.js'),
 	extension: require('./extension.js'),
 	i18n: require('./i18n.js'),
 	runtime: require('./runtime.js'),
