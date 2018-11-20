db929b83a8ea085943ce56d229c1cf30f9409555
Studený Pavel <studeny@avast.com>
2018-11-20 14:56:16 +0100

    chrome.windows.WINDOWS_ID_CURRENT

--- src/Framework/Sources/JavaScript/Background/chrome/index.js
+++ src/Framework/Sources/JavaScript/Background/chrome/index.js
@@ -5,5 +5,6 @@ module.exports = {
 	extension: require('./extension.js'),
 	i18n: require('./i18n.js'),
 	runtime: require('./runtime.js'),
-	tabs: require('./tabs.js')
+	tabs: require('./tabs.js'),
+	windows: require('./windows.js')
 };
---
+++ src/Framework/Sources/JavaScript/Background/chrome/windows.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var windows = {
+    WINDOW_ID_NONE: -1,
+    WINDOW_ID_CURRENT: -2
+};
+
+module.exports = windows;
